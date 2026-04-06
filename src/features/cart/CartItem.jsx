d1ab6bcf98d9d666ade2908ab4c2f0b00e7d9cf4
Nasir <nasiryousuf200511@gmail.com>
2026-04-06 17:53:33 +0600

    chore: add vercel.json to fix react-router 404s

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -5,13 +5,10 @@ import { useDispatch, useSelector } from "react-redux";
 import DeleteItem from "./DeleteItem";
 import UpdateItemQuantity from "./UpdateItemQuantity";
 function CartItem({ item }) {
-  const dispatch = useDispatch();
   const { pizzaId, name, quantity, totalPrice } = item;
-  function handleDeleteItem(id) {
-    dispatch(deleteItem(id));
-  }
 
   const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
+
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
